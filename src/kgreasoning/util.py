c3abac3965e8699cddf82c5f8fa35faef20dc700
Jung Dongwon <dg52316@naver.com>
2021-11-05 19:31:39 +0900

    Add files via upload

---
+++ src/kgreasoning/util.py
@@ -0,0 +1,78 @@
+import numpy as np
+import random
+import torch
+import time
+
+def list2tuple(l):
+    return tuple(list2tuple(x) if type(x)==list else x for x in l)
+
+def tuple2list(t):
+    return list(tuple2list(x) if type(x)==tuple else x for x in t)
+
+flatten=lambda l: sum(map(flatten, l),[]) if isinstance(l,tuple) else [l]
+
+def parse_time():
+    return time.strftime("%Y.%m.%d-%H:%M:%S", time.localtime())
+
+def set_global_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic=True
+
+def eval_tuple(arg_return):
+    """Evaluate a tuple string into a tuple."""
+    if type(arg_return) == tuple:
+        return arg_return
+    if arg_return[0] not in ["(", "["]:
+        arg_return = eval(arg_return)
+    else:
+        splitted = arg_return[1:-1].split(",")
+        List = []
+        for item in splitted:
+            try:
+                item = eval(item)
+            except:
+                pass
+            if item == "":
+                continue
+            List.append(item)
+        arg_return = tuple(List)
+    return arg_return
+
+def flatten_query(queries):
+    all_queries = []
+    for query_structure in queries:
+        tmp_queries = list(queries[query_structure])
+        all_queries.extend([(query, query_structure) for query in tmp_queries])
+    return all_queries
+
+def dcg_at_k(r, k, method=1):
+    """Score is discounted cumulative gain (dcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Returns:
+        Discounted cumulative gain
+    """
+    r = np.asfarray(r)[:k]
+    if r.size:
+        if method == 0:
+            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+        elif method == 1:
+            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+        else:
+            raise ValueError('method must be 0 or 1.')
+    return 0.
+
+def ndcg_at_k(r, k, method=1):
+    """Score is normalized discounted cumulative gain (ndcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Returns:
+        Normalized discounted cumulative gain
+    """
+    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    if not dcg_max:
+        return 0.
+    return dcg_at_k(r, k, method) / dcg_max
